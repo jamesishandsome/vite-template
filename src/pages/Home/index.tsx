d15c34ac6bcf4ee7abdff60fd396bd4d8f6f3a89
jameshu <james.hu@barecovetech.com>
2023-10-04 17:03:36 +0800

    feature: add base url for router

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,4 @@
-import reactLogo from '@/assets/react.svg';
 import { counter } from '@/reducer';
-import viteLogo from '/vite.svg';
 import './style.css';
 
 import { useAppDispatch, useAppSelector } from '@/hooks';
@@ -10,15 +8,6 @@ function Home() {
   const dispatch = useAppDispatch();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => dispatch(counter.increment())}>
           count is {count}
@@ -27,9 +16,6 @@ function Home() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,13 +4,16 @@ import { Home } from '@/pages/Home';
 import { ErrorPage } from '@/pages/Error';
 
 const Router: React.FC = () => {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />,
-      errorElement: <ErrorPage />,
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <Home />,
+        errorElement: <ErrorPage />,
+      },
+    ],
+    { basename: import.meta.env.BASE_URL },
+  );
   return <RouterProvider router={router} />;
 };
 
